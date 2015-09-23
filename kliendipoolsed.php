f9e91d2fb63c5fe2ba2d193113058ba735d6d9e3
Morten <morten.ehari@khk.ee>
2015-09-23 12:37:25 +0300

    1434064-103883684 5. Kass koeraks

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -5,9 +5,13 @@
         <title>Document</title>
     </head>
     <body>
+
+    <!-- Tere maailm alert -->
     <button onclick="TereMaailmButton()">
         Tere maailm
     </button>
+
+    <!-- Tere maailm link -->
 
     <script>
             function TereMaailmButton() {
@@ -24,6 +28,23 @@
     </script>
 
     <a href="javascript:TereMaailmKHKee();">Tere maailm</a>
+
+    <!-- Jääme siia tag -->
     <a href="#" onClick="alert('Jääme siia')">Jääme siia</a>
+    <br>
+    <br>
+
+    <!-- kassi koera pildi vahetus script -->
+    <img id="catdog" onclick="changeImage()" src="images/cat.png" width="200" height="200">
+    <script type="text/javascript">
+        function changeImage() {
+            var image = document.getElementById('catdog');
+            if (image.src.match("dog")) {
+                image.src = "images/cat.png";
+            } else {
+                image.src = "images/dog.jpg";
+            }
+        }
+    </script>
     </body>
 </html>
